a060744cfec9f8ac817eedb245d8b8001e1a3248
Ankan Das <113364356+ankandas101@users.noreply.github.com>
2024-01-12 10:59:48 +0600

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,46 @@
+<?php
+include('config.php');
+
+$id = $_GET['id'];
+
+if(isset($_POST['submit']))
+{
+	$u_card = $_POST['card_no'];
+	$u_f_name = $_POST['user_first_name'];
+	$u_l_name = $_POST['user_last_name'];
+	$u_father = $_POST['user_father'];
+	$u_nid = $_POST['user_nid'];
+	$u_birthday = $_POST['user_dob'];
+	$u_gender = $_POST['user_gender'];
+	$u_email = $_POST['user_email'];
+	$u_phone = $_POST['user_phone'];
+	$u_division = $_POST['division'];
+	$u_dist = $_POST['dist'];
+	$u_village = $_POST['village'];
+	$u_police = $_POST['police_station'];
+	$u_pincode = $_POST['pincode'];
+	$u_mother = $_POST['user_mother'];
+	$u_details = $_POST['details'];
+	$u_staff_id = $_POST['staff_id'];
+	
+	$msg = "";
+	$image = $_FILES['image']['name'];
+	$target = "upload_images/".basename($image);
+
+	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+
+	$update = "UPDATE student_data SET u_card='$u_card', u_f_name = '$u_f_name', u_l_name = '$u_l_name', u_father = '$u_father', u_mother = '$u_mother', u_nid = '$u_nid', u_birthday = '$u_birthday', u_gender = '$u_gender', u_email = '$u_email', u_phone = '$u_phone', u_division = '$u_division', u_dist = '$u_dist', u_village = '$u_village', u_police = '$u_police', u_pincode = '$u_pincode', u_details = '$u_details', staff_id = '$u_staff_id', image = '$image' WHERE id=$id ";
+	$run_update = mysqli_query($con,$update);
+
+	if($run_update){
+		header('location:index.php');
+	}else{
+		echo "Data not update";
+	}
+}
+
+?>
